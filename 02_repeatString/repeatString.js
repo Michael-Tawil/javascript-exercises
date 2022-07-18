fbe6b917ff89f1166aae39bc5e0c4333f4038bea
Michael-Tawil <michaeltawil3@gmail.com>
2022-07-18 20:58:11 +1000

    repeat string done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
+const repeatString = function(word, time) {
 
+    let msg ="";
+
+    if (time < 0){
+
+            return "ERROR";
+        }
+        
+    else{
+        for (let i = 0; i < time; i++){
+
+            msg += word;
+        }
+
+    return  msg;
+    }
+    
 };
 
 // Do not edit below this line
