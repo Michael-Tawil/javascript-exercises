c8f16a40a9bc6a68f028a7f7c47c169de38c4747
Michael-Tawil <michaeltawil3@gmail.com>
2022-08-12 12:43:09 +1000

    sum all solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(startNum,endNum) {
 
+let sum   = 0;
+
+if(typeof(endNum) === "number" && typeof(startNum) === "number"){
+if (startNum < 0 || endNum < 0 ){
+
+    return "ERROR"
+}else{
+    
+    if(endNum > startNum){
+
+        for(let i = 0; i<= endNum; i++){
+
+            sum += i;
+        }
+        return sum
+    }
+    else if (startNum > endNum){
+
+        for(let i = 0; i<= startNum; i++){
+
+            sum += i;
+        }
+        return sum
+    }
+}
+}else{
+
+    return "ERROR"
+}
 };
 
 // Do not edit below this line
