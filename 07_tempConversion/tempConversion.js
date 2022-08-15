9c50406a5d0733d7546fb9f1d726841e0b1054a7
Michael-Tawil <michaeltawil3@gmail.com>
2022-08-15 21:04:04 +1000

    solved tempconv

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const ftoc = function() {
+const ftoc = function(degree) {
+
+  degree = (degree - 32) * 0.55555555555556;
+
+  return Math.round(degree * 10) / 10
 
 };
 
-const ctof = function() {
+const ctof = function(degree) {
 
+  degree = degree * 1.8 + 32;
+
+  return Math.round(degree * 10) / 10
 };
 
 // Do not edit below this line
